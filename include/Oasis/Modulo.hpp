88cea30b0824071dd16603a5f04a2c9bd2646f87
Marcuschan2004 <mchan.marcus@gmail.com>
2024-08-10 19:12:46 -0400

    commit

--- include/Oasis/Modulo.hpp
+++ include/Oasis/Modulo.hpp
@@ -2,6 +2,7 @@
 #define OASIS_MODULO_HPP
 
 #include "BinaryExpression.hpp"
+#include "Constant.hpp"
 
 namespace Oasis {
 
@@ -18,123 +19,23 @@ public:
     Modulo(const Expression& dividend, const Expression& divisor)
         : BinaryExpression(dividend, divisor) {}
 
-    [[nodiscard]] auto Simplify() const -> std::unique_ptr<Expression> final {
-        if (this->GetLeastSigOp().IsZero()) {
-            throw std::runtime_error("Division by zero in modulo operation.");
-        }
+    [[nodiscard]] auto Simplify() const -> std::unique_ptr<Expression> final;
+    auto Simplify(tf::Subflow& subflow) const -> std::unique_ptr<Expression> final;
+    [[nodiscard]] auto Differentiate(const Expression& differentiationVariable) const -> std::unique_ptr<Expression> final;
+    [[nodiscard]] auto Integrate(const Expression& integrationVariable) -> std::unique_ptr<Expression> final;
 
-        if (this->GetMostSigOp().IsZero()) {
-            return std::make_unique<Constant>(0);
-        }
+    [[nodiscard]] auto Evaluate(const std::unordered_map<std::string, double>& variables) const -> double final;
+    [[nodiscard]] auto Compare(const Expression& other) const -> bool final;
+    [[nodiscard]] auto Clone() const -> std::unique_ptr<Expression> final;
 
-        if (this->GetLeastSigOp().IsOne()) {
-            return std::make_unique<Constant>(0);
-        }
-
-        return nullptr; // No further simplification possible
-    }
-
-    auto Simplify(tf::Subflow& subflow) const -> std::unique_ptr<Expression> final {
-        auto result = std::make_unique<Modulo>(*this);
-
-        subflow.emplace([&]() {
-            auto simplifiedDividend = this->GetMostSigOp().Simplify(subflow);
-            result->SetMostSigOp(*simplifiedDividend);
-        });
-
-        subflow.emplace([&]() {
-            auto simplifiedDivisor = this->GetLeastSigOp().Simplify(subflow);
-            result->SetLeastSigOp(*simplifiedDivisor);
-        });
-
-        subflow.join();
-
-        return result->Simplify(); // Simplify again after parallel tasks
-    }
-
-    [[nodiscard]] auto Differentiate(const Expression& differentiationVariable) const -> std::unique_ptr<Expression> final {
-        auto u = this->GetMostSigOp();
-        auto v = this->GetLeastSigOp();
-        auto du = u.Differentiate(differentiationVariable);
-        auto dv = v.Differentiate(differentiationVariable);
-
-        auto quotient = std::make_unique<Divide>(u, v);
-        auto quotientDerivative = quotient->Differentiate(differentiationVariable);
-
-        auto result = std::make_unique<Subtract>(du, std::make_unique<Multiply>(dv, quotientDerivative));
-
-        return result;
-    }
-
-    [[nodiscard]] auto Integrate(const Expression& integrationVariable) -> std::unique_ptr<Expression> final {
-        throw std::logic_error("Integration of modulo operation is not generally defined.");
-    }
-
-    static auto Specialize(const Expression& other) -> std::unique_ptr<Modulo> {
-        if (!other.Is<Oasis::Modulo>()) {
-            return nullptr;
-        }
-        auto specialized = std::make_unique<Modulo>();
-
-        std::unique_ptr<Expression> otherGeneralized = other.Generalize();
-        const auto& otherBinaryExpression = static_cast<const Modulo<Expression>&>(*otherGeneralized);
-
-        if (otherBinaryExpression.HasMostSigOp()) {
-            specialized->SetMostSigOp(*Expression::Specialize(otherBinaryExpression.GetMostSigOp()));
-        }
-
-        if (otherBinaryExpression.HasLeastSigOp()) {
-            specialized->SetLeastSigOp(*Expression::Specialize(otherBinaryExpression.GetLeastSigOp()));
-        }
-
-        return specialized;
-    }
-
-    static auto Specialize(const Expression& other, tf::Subflow& subflow) -> std::unique_ptr<Modulo> {
-        if (!other.Is<Oasis::Modulo>()) {
-            return nullptr;
-        }
-        Modulo specialized;
-
-        std::unique_ptr<Expression> otherGeneralized;
-        tf::Task generalizeTask = subflow.emplace([&other, &otherGeneralized](tf::Subflow& sbf) {
-            otherGeneralized = other.Generalize(sbf);
-        });
-
-        tf::Task mostSigOpTask = subflow.emplace([&specialized, &otherGeneralized](tf::Subflow& sbf) {
-            const auto& otherBinaryExpression = dynamic_cast<const Modulo<Expression>&>(*otherGeneralized);
-            if (otherBinaryExpression.HasMostSigOp()) {
-                specialized.SetMostSigOp(*Expression::Specialize(otherBinaryExpression.GetMostSigOp(), sbf));
-            }
-        });
-
-        mostSigOpTask.succeed(generalizeTask);
-
-        tf::Task leastSigOpTask = subflow.emplace([&specialized, &otherGeneralized](tf::Subflow& sbf) {
-            const auto& otherBinaryExpression = dynamic_cast<const Modulo<Expression>&>(*otherGeneralized);
-            if (otherBinaryExpression.HasLeastSigOp()) {
-                specialized.SetLeastSigOp(*Expression::Specialize(otherBinaryExpression.GetLeastSigOp(), sbf));
-            }
-        });
-
-        leastSigOpTask.succeed(generalizeTask);
-
-        subflow.join();
-
-        return std::make_unique<Modulo>(specialized);
-    }
+    static auto Specialize(const Expression& other) -> std::unique_ptr<Modulo>;
+    static auto Specialize(const Expression& other, tf::Subflow& subflow) -> std::unique_ptr<Modulo>;
 
     EXPRESSION_TYPE(Modulo)
     EXPRESSION_CATEGORY(BinExp)
 };
 /// @endcond
 
-/**
- * The Modulo expression calculates the remainder of the division of two expressions.
- *
- * @tparam DividendT The expression to be divided.
- * @tparam DivisorT The expression to divide the dividend by.
- */
 template <IExpression DividendT = Expression, IExpression DivisorT = DividendT>
 class Modulo : public BinaryExpression<Modulo, DividendT, DivisorT> {
 public:
@@ -156,3 +57,4 @@ public:
 } // Oasis
 
 #endif // OASIS_MODULO_HPP
+
--- src/Modulo.cpp
+++ src/Modulo.cpp
@@ -1,4 +1,7 @@
 #include "Modulo.hpp"
+#include "Divide.hpp"
+#include "Subtract.hpp"
+#include "Multiply.hpp"
 
 namespace Oasis {
 
@@ -16,6 +19,18 @@ auto Modulo<Expression, Expression>::Simplify() const -> std::unique_ptr<Express
 
     if (this->GetLeastSigOp().IsOne()) {
         return std::make_unique<Constant>(0);
+    }
+
+    if (this->GetMostSigOp() == this->GetLeastSigOp()) {
+        return std::make_unique<Constant>(0); // x % x = 0
+    }
+
+    // Simplify both sides if possible
+    auto simplifiedDividend = this->GetMostSigOp().Simplify();
+    auto simplifiedDivisor = this->GetLeastSigOp().Simplify();
+
+    if (simplifiedDividend && simplifiedDivisor) {
+        return std::make_unique<Modulo<Expression, Expression>>(*simplifiedDividend, *simplifiedDivisor);
     }
 
     return nullptr; // No further simplification possible
@@ -45,16 +60,45 @@ auto Modulo<Expression, Expression>::Differentiate(const Expression& differentia
     auto du = u.Differentiate(differentiationVariable);
     auto dv = v.Differentiate(differentiationVariable);
 
+    // Modulo differentiation uses the following general formula:
+    // d(u % v)/dx = du - (v * floor(u/v) * dv + u * dv / v)
     auto quotient = std::make_unique<Divide>(u, v);
+    auto floorQuotient = std::make_unique<Floor>(*quotient);
     auto quotientDerivative = quotient->Differentiate(differentiationVariable);
-
-    auto result = std::make_unique<Subtract>(du, std::make_unique<Multiply>(dv, quotientDerivative));
+    auto term1 = std::make_unique<Multiply>(v, *floorQuotient);
+    auto term2 = std::make_unique<Multiply>(term1, dv);
+    auto result = std::make_unique<Subtract>(du, std::make_unique<Divide>(term2, v));
 
     return result;
 }
 
 auto Modulo<Expression, Expression>::Integrate(const Expression& integrationVariable) -> std::unique_ptr<Expression> {
     throw std::logic_error("Integration of modulo operation is not generally defined.");
+}
+
+auto Modulo<Expression, Expression>::Evaluate(const std::unordered_map<std::string, double>& variables) const -> double {
+    double dividendValue = this->GetMostSigOp().Evaluate(variables);
+    double divisorValue = this->GetLeastSigOp().Evaluate(variables);
+
+    if (divisorValue == 0) {
+        throw std::runtime_error("Division by zero in modulo operation.");
+    }
+
+    return std::fmod(dividendValue, divisorValue);
+}
+
+auto Modulo<Expression, Expression>::Compare(const Expression& other) const -> bool {
+    if (!other.Is<Modulo>()) {
+        return false;
+    }
+
+    const auto& otherModulo = static_cast<const Modulo<Expression, Expression>&>(other);
+    return this->GetMostSigOp().Compare(otherModulo.GetMostSigOp()) &&
+           this->GetLeastSigOp().Compare(otherModulo.GetLeastSigOp());
+}
+
+auto Modulo<Expression, Expression>::Clone() const -> std::unique_ptr<Expression> {
+    return std::make_unique<Modulo<Expression, Expression>>(this->GetMostSigOp(), this->GetLeastSigOp());
 }
 
 auto Modulo<Expression, Expression>::Specialize(const Expression& other) -> std::unique_ptr<Modulo> {
@@ -112,3 +156,4 @@ auto Modulo<Expression, Expression>::Specialize(const Expression& other, tf::Sub
 }
 
 } // namespace Oasis
+
